d3c79d60215dd07d1ef8441be630f2d7b330d6e2
Hepsibaalkolu <hepsiba215@gmail.com>
2025-05-09 13:00:56 +0530

    Update server.js and .env for Render deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import gameRoutes from "./routes/gameRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
